fe5cd20c7acee331d46a060c5bb075443586dd90
sumanthgunna <sumanthgunna@missionrnd.com>
2016-03-20 14:41:04 +0530

    Submit Commit

--- src/twodigitLinkedList.cpp
+++ src/twodigitLinkedList.cpp
@@ -22,5 +22,11 @@ struct node {
 };
 
 int convert_sll_2digit_to_int(struct node *head){
-	return 0;
+	int sum = 0;
+	while (head != NULL)
+	{
+		sum = sum * 100 + ((head->digit1 * 10) + (head->digit2 * 1));
+		head = head->next;
+	}
+	return sum;
 }
